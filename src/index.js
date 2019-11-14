75cb1eb2dbdbeb0fe101b20138ea755ba42ae7b9
Aarif Shaikh <Aarif.Shaikh@sony.com>
2019-11-14 14:52:05 +0900

    added faker library to generate random data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
     return(
-        <h1>setting things up</h1>
+        <div className="ui container comments"> 
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}></img>
+                </a>
+                <div className="content">
+                    <a href="/.."className="author"> Sam </a>
+                    <div className="metadata">
+                        <span className="date"> Today at 5:23pm</span>
+                    </div>
+                    <div className="text" > haha</div>
+
+                </div>
+            </div>
+        </div>
     ) 
         
 };
