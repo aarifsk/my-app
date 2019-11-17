87090b3d0e0d611af366e53e87b921a163ce780f
aarifsk <aarifsk1618@gmail.com>
2019-11-17 19:00:52 +0900

    bg for parent

---
+++ src/component/ViewAnimation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View} from 'react-native';
+
+export default class extends React.Component{
+    render() {
+        return(
+            <View style={{backgroundColor="green"}}>
+                <View>
+                    This is hidden view
+                </View>
+            </View>
+        );
+    }
+}
