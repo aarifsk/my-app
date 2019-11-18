6d03b23eed2d8ab0f488868e55156db97ab38675
Aarif Shaikh <Aarif.Shaikh@sony.com>
2019-11-18 15:45:25 +0900

    clean up

--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
 
 const App = () => {
     return(
-        <div className="ui container comments"> 
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}></img>
-                </a>
-                <div className="content">
-                    <a href="/.."className="author"> Sam </a>
-                    <div className="metadata">
-                        <span className="date"> Today at 5:23pm</span>
-                    </div>
-                    <div className="text" > haha</div>
-
-                </div>
-            </div>
-        </div>
+        <div></div>
     ) 
         
 };
